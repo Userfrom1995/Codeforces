a550d7905535dc5684631c940a78f6f312d78770
USER1995 <userfrom1995@gmail.com>
2024-10-01 12:10:37 +0530

    Added More problems

--- BitwiseBalancing.cpp
+++ BitwiseBalancing.cpp
@@ -1,45 +1,52 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-// Function to check if a given `a` satisfies the equation
-long long  satisfiesEquation(long long a, long long b, long long c) {
-    return ((a | b) - (a & c));
-}
-
-
-void findA() {
-     long long b, c, d;
-    cin >> b >> c >> d;
-    long long left = 1;
-    long long right = (1LL << 61); 
-    long long answer = -1;  
-
-    while (left <= right) {
-        long long mid = left + (right - left) / 2;
-
+void solve() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        long long b, c, d;
+        cin >> b >> c >> d;
         
-        if (satisfiesEquation(mid, b, c)==d) {
-            answer = mid;  
-            right = mid - 1; 
-            break;
-        } else {
-            left = mid + 1; 
-        }
+        long long a = 0;  // candidate solution for 'a'
+        bool possible = true;
+        
+        // Check each bit position from 0 to 61
+        for (int i = 0; i < 62; ++i) {
+            int bit_b = (b >> i) & 1;
+            int bit_c = (c >> i) & 1;
+            int bit_d = (d >> i) & 1;
+
+            if((1|bit_b)-(1&bit_c)==bit_d) {
+                a |= (1LL << i);
+            }
+            else if((0|bit_b)-(0&bit_c)==bit_d) {
+                a &= ~(1LL << i);
+            }
+            else {
+                possible = false;
+                break;
+            }
+            
+           
+         
+    }
+    if(possible) {
+        cout << a << endl;
+    }
+    else {
+        cout << -1 << endl;
     }
 
-    cout<<answer<<"\n";
+}
 }
 
 int main() {
-    // Example usage
-    long long a;
-    cin >> a;
-    while(a--){
-   
-
-    findA();
-    }
-
-   
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    solve();
+    
     return 0;
 }
